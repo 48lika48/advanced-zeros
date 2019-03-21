549362b3ef8f3ec5b42cc876006aeaf249669777
48lika48 <48lika48@mail.ru>
2019-03-21 12:32:26 +0300

    feat: add initial function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  
+  var counter = 0;
+  for (var i = 5; number/i >= 1; i *= 5)
+    counter += Math.floor(number/i);
+  return counter;
 }
+  
